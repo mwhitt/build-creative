d10d1eea4d42182cad47deb89069027d17a1f91c
Matthew Whittaker <mwhitt89@gmail.com>
2015-09-30 08:52:16 -0700

    give space on mobile

--- about.php
+++ about.php
@@ -18,28 +18,28 @@ get_header();
   </section>
 
   <section class="about-us">
-    <div class="clearfix">
+    <div class="clearfix single-member">
       <img src="<?php bloginfo('template_directory'); ?>/images/Elliot.jpg" />
       <span class="about-name">Elliot Moore</span>
       <span class="about-title">Co-Founder / Business Development</span>
       <p>Elliot is the creative liaison between some of the largest conglomerates in Silicon Valley and the Creative Expertise of the Build Creative. When he isn&rsquo;t drinking IPA&rsquo;s with prospects, he is directing and producing projects for The Build. Otherwise he&rsquo;s exploring and crafting his own narrative stories.</p>
     </div>
 
-    <div class="clearfix">
+    <div class="clearfix single-member">
       <img src="<?php bloginfo('template_directory'); ?>/images/Sharif.jpg" />
       <span class="about-name">Sharif Nakhleh</span>
       <span class="about-title">Co-Founder / Creative Director</span>
       <p>Sharif creates concepts, directs, and edits videos at The Build. He&rsquo;s been at it for over 15 years and is versed in the entire process of content creation from concept to web. When he&rsquo;s not distilling a feeling or idea down to its root, he&rsquo;s usually thinking of new ways to tell a story... preferably ones that feature cats, so that he may gain their respect. In the end it&rsquo;s about respect.</p>
     </div>
 
-    <div class="clearfix">
+    <div class="clearfix single-member">
       <img src="<?php bloginfo('template_directory'); ?>/images/Mike.jpg" />
       <span class="about-name">Michael Epple</span>
       <span class="about-title">Co-Founder / Visual Director</span>
       <p>Mike has been a Director of Photography and creative for over 10 years. He has worked extensively with ad agencies and production companies alike. But basically he likes looking at things, for a long time. So long that it gets uncomfortable, until something else shiny distracts him.</p>
     </div>
 
-    <div class="clearfix">
+    <div class="clearfix single-member">
       <img src="<?php bloginfo('template_directory'); ?>/images/Jack.jpg" />
       <span class="about-name">Jack Birmingham</span>
       <span class="about-title">Co-Founder / Lead Copywriter</span>
